495d1929c1666656d4fb43e91d4a2c1cd3d8f69e
Connor Blackler <connor.blackler.ss@gmail.com>
2023-05-10 15:25:54 +0100

    refactored html, adding nav to each, updated to use objective formatting

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.item_list),
+    path('', views.home_page, name="home"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('products/', views.product_search_page, name="products"),
+    path('product/<str:pk>', views.single_product_page, name="product"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,9 +4,29 @@ from .models import Item
 # Create your views here.
 
 
-def item_list(request):
+def home_page(request):
+    context = {}
+
+    return render(request, "home-page.html", context)
+
+
+def checkout(request):
+    context = {}
+
+    return render(request, "checkout-page.html", context)
+
+
+def product_search_page(request):
     context = {
         "items": Item.objects.all()
     }
 
-    return render(request, "home-page.html", context)
+    return render(request, "product-search-page.html", context)
+
+
+def single_product_page(request, pk):
+    context = {
+        "item": Item.objects.all()
+    }
+
+    return render(request, "product-page.html", context)
